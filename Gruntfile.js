393159cee47087b7d7d6a69c929930d9fa927483
Hack Reactor Students <accounts@hackreactor.com>
2015-04-25 15:57:33 -0700

    Rebuilt the gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,17 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
+      options: {
+        separator: ';'
+      },
       dist: {
-        src: ['public/client/*.js'],
-        dest: 'public/client/concatClient.js',
+        src: [
+        'public/lib/*.js',
+        'public/client/*.js'
+        ],
+        dest: 'public/dist/unified.js', // should this be in client?
       }
     },
 
@@ -26,15 +33,18 @@ module.exports = function(grunt) {
 
     uglify: {
       build: {
-        src: 'public/client/concatClient.js',
-        dest: 'public/client/uglifyClient.min.js'
+        src: 'public/dist/unified.js',
+        dest: 'public/dist/uglified.js'
       }
     },
 
     jshint: {
-      files: ['public/client/*.js'],
+      files: [
+        'public/lib/*.js',
+        'public/client/*.js'
+      ],
       options: {
-        force: 'false',
+        force: 'true',
         jshintrc: '.jshintrc',
         ignores: [
           'public/lib/**/*.js',
@@ -99,20 +109,141 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  // concatenate files, uglify code, jshint before deployment
-  grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('build', [ 'concat', 'uglify'
+  ]);
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
-      grunt.task.run(['test', 'build']);
+      grunt.task.run([' shell.prodServer ']);
       // add your production server task here
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
-  grunt.registerTask('deploy', ['mochaTest', 'jshint', 'concat', 'uglify']);
-  //grunt.registerTask('deploy', ['upload']);
+  grunt.registerTask('deploy', ['jshint', 'test', 'build'
+    // add your deploy tasks here
+  ]);
 
 
 };
+
+
+// module.exports = function(grunt) {
+
+//   grunt.initConfig({
+//     pkg: grunt.file.readJSON('package.json'),
+//     concat: {
+//       dist: {
+//         src: ['public/client/*.js'],
+//         dest: 'public/client/concatClient.js',
+//       }
+//     },
+
+//     mochaTest: {
+//       test: {
+//         options: {
+//           reporter: 'spec'
+//         },
+//         src: ['test/**/*.js']
+//       }
+//     },
+
+//     nodemon: {
+//       dev: {
+//         script: 'server.js'
+//       }
+//     },
+
+//     uglify: {
+//       build: {
+//         src: 'public/client/concatClient.js',
+//         dest: 'public/client/uglifyClient.min.js'
+//       }
+//     },
+
+//     jshint: {
+//       files: ['public/client/*.js'],
+//       options: {
+//         force: 'false',
+//         jshintrc: '.jshintrc',
+//         ignores: [
+//           'public/lib/**/*.js',
+//           'public/dist/**/*.js'
+//         ]
+//       }
+//     },
+
+//     cssmin: {
+//     },
+
+//     watch: {
+//       scripts: {
+//         files: [
+//           'public/client/**/*.js',
+//           'public/lib/**/*.js',
+//         ],
+//         tasks: [
+//           'concat',
+//           'uglify'
+//         ]
+//       },
+//       css: {
+//         files: 'public/*.css',
+//         tasks: ['cssmin']
+//       }
+//     },
+
+//     shell: {
+//       prodServer: {
+//       }
+//     },
+//   });
+
+//   grunt.loadNpmTasks('grunt-contrib-uglify');
+//   grunt.loadNpmTasks('grunt-contrib-jshint');
+//   grunt.loadNpmTasks('grunt-contrib-watch');
+//   grunt.loadNpmTasks('grunt-contrib-concat');
+//   grunt.loadNpmTasks('grunt-contrib-cssmin');
+//   grunt.loadNpmTasks('grunt-mocha-test');
+//   grunt.loadNpmTasks('grunt-shell');
+//   grunt.loadNpmTasks('grunt-nodemon');
+
+//   grunt.registerTask('server-dev', function (target) {
+//     // Running nodejs in a different process and displaying output on the main console
+//     var nodemon = grunt.util.spawn({
+//          cmd: 'grunt',
+//          grunt: true,
+//          args: 'nodemon'
+//     });
+//     nodemon.stdout.pipe(process.stdout);
+//     nodemon.stderr.pipe(process.stderr);
+
+//     grunt.task.run([ 'watch' ]);
+//   });
+
+//   ////////////////////////////////////////////////////
+//   // Main grunt tasks
+//   ////////////////////////////////////////////////////
+
+//   grunt.registerTask('test', [
+//     'mochaTest'
+//   ]);
+
+//   // concatenate files, uglify code, jshint before deployment
+//   grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+
+//   grunt.registerTask('upload', function(n) {
+//     if(grunt.option('prod')) {
+//       grunt.task.run(['test', 'build']);
+//       // add your production server task here
+//     } else {
+//       grunt.task.run([ 'server-dev' ]);
+//     }
+//   });
+
+//   grunt.registerTask('deploy', ['mochaTest', 'jshint', 'concat', 'uglify']);
+//   //grunt.registerTask('deploy', ['upload']);
+
+
+// };
